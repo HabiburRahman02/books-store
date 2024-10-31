915b5391cfd67a6e529558c1bf0f733a513661e6
Habibur Rahman <habibur88454@gmail.com>
2024-10-31 20:50:23 +0600

    single books added

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
     const [books, setBooks] = useState([]);
@@ -11,8 +12,12 @@ const Books = () => {
 
     return (
         <div className="mb-14">
-            <h3 className="text-3xl font-semibold text-center">Books</h3>
-            {books.length}
+            <h3 className="text-3xl font-semibold text-center mb-8">Books</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
+                }
+            </div>
         </div>
     );
 };
