81a2f302b33f1046092c972836e5ac488df97025
Habibur Rahman <habibur88454@gmail.com>
2024-10-31 19:53:05 +0600

    added footer

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const Main = () => {
     return (
@@ -8,6 +9,7 @@ const Main = () => {
             <div className="bg-red-200">
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
