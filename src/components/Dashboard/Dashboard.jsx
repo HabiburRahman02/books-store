05ca80f3a7da35f4ea4f182dd14f55234546fe46
Habibur Rahman <habibur88454@gmail.com>
2024-11-01 19:24:15 +0600

    barchart added using year and pages

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BookContext } from '../context/BookContextMain';
+
 
 const Dashboard = () => {
-    return (
-        <div>
+    const { markBooks } = useContext(BookContext);
 
+    return (
+        <div className='my-14 h-[500px] md:w-1/2 mx-auto'>
+            <ResponsiveContainer>
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={markBooks}
+                >
+                    <XAxis dataKey="yearOfPublishing" />
+                    <YAxis dataKey='totalPages' />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="totalPages" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                    <Bar dataKey="yearOfPublishing" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
